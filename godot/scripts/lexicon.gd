6590ac181c87350445b1f2da4ad3a3cecf047f6d
mirloom <mirlacardenal@gmail.com>
2024-11-22 19:05:36 -0500

    revert look and feel back prior to no-caching test. It did work

--- godot/scripts/lexicon.gd
+++ godot/scripts/lexicon.gd
@@ -67,7 +67,7 @@ func _process(delta: float) -> void:
 
 
 func _reset() -> void:
-	background.color = Color.RED
+	background.color = Cosmetics.DARK_GRAY_COLOR
 	continuation_container.visible = false
 	_clue_count_used_in_current_word = 0
 	
