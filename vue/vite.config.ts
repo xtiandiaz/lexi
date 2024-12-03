21ea4eac506a23ee58a92204a0f955d6ecc96f2c
Cristian Díaz <xtndiaz@gmail.com>
2024-12-03 14:51:17 -0500

    Agregar nuevas palabras. Remove unnecessary line at vite.config

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/lexicon/vue/dist/",
-  // publicDir: process.env.NODE_ENV === "production" ? "/lexicon/" : "/",
   plugins: [
     vue(),
     vueDevTools(),
