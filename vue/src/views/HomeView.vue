8f34b0c92900a6e3fbcedc40fce2df41dad78c11
Cristian Díaz <xtndiaz@gmail.com>
2024-12-05 17:39:51 -0500

    Load wording file using Fetch

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 // import TheWelcome from '../components/TheWelcome.vue'
-import wordListing from '../words/es.txt?raw'
-const words = ref(wordListing.split('\n'))
+// import wordListing from '../words/es.txt?raw'
+
+const words = ref<string[]>()
+
+async function load() {
+  try {
+    const response = await fetch("./words/es.txt")
+    console.log(response)
+    const wordListing = await response.text()
+    console.log(wordListing)
+    words.value = wordListing.split('\n')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+load()
 </script>
 
 <template>
