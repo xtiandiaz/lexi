9e86b48889b83b7c569f92f7e0a64ad984878f4a
Cristian Díaz <xtndiaz@gmail.com>
2025-02-04 11:09:58 -0500

    Add ES excerpts

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -17,9 +17,9 @@ function resetActiveWord() {
 async function load() {
   try {
     const response = await fetch(`https://raw.githubusercontent.com/xtiandiaz/lexicon/refs/heads/main/words/es.txt?salt=${Math.random()}`)
-    console.log(response)
+    // console.log(response)
     const wordListing = await response.text()
-    console.log(wordListing)
+    // console.log(wordListing)
     words.value = wordListing.split('\n')
     resetActiveWord()
   } catch (error) {
@@ -34,6 +34,6 @@ load()
   <!-- <main>     -->
     <h1 id="slate" class="serif" v-if="(words?.length ?? 0) != 0">{{ activeWord }}</h1>
     <button class="emoji" v-on:click="resetActiveWord">🦄</button>
-    <span class="caption all-caps">©2025 XD</span>
+    <span class="strong serif">©2025 XD</span>
   <!-- </main> -->
 </template>
