27438b3ad9321336b5384137d5372bb9801155f8
Cristian Díaz <xtndiaz@gmail.com>
2025-02-12 20:49:44 -0500

    Add more-info buttons for active word

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -3,6 +3,12 @@ import { ref } from 'vue'
 // import TheWelcome from '../components/TheWelcome.vue'
 // import wordListing from '../words/es.txt?raw'
 
+enum WordAction {
+  Define, 
+  Search,
+  SearchForImages
+}
+
 const words = ref<string[]>()
 const activeWord = ref<string>()
 
@@ -26,14 +32,45 @@ async function load() {
     console.log(error)
   }
 }
-
 load()
+
+function runWordAction(action: WordAction, word: string | undefined): void {
+  if (word === undefined) {
+    return
+  }
+  
+  const url = (() => {
+    switch (action) {
+      case WordAction.Define:
+        return `https://dle.rae.es/${word}`
+      case WordAction.Search:
+        return `https://duckduckgo.com/?t=ffab&q=${activeWord.value}`
+      case WordAction.SearchForImages:
+        return `https://duckduckgo.com/?t=ffab&q=${activeWord.value}&iax=images&ia=images`
+    }
+  })()
+  
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
-  <!-- <main>     -->
+  <!-- <main> -->
     <h1 id="slate" class="serif" v-if="(words?.length ?? 0) != 0">{{ activeWord }}</h1>
-    <button class="emoji" v-on:click="resetActiveWord">🦄</button>
+    <div class="button-bar">
+      <button class="iconized" v-on:click="runWordAction(WordAction.Define, activeWord)">
+        <span class="icon define"></span>
+      </button>
+      <button class="iconized" v-on:click="runWordAction(WordAction.Search, activeWord)">
+        <span class="icon search"></span>
+      </button>
+      <button class="iconized" v-on:click="runWordAction(WordAction.SearchForImages, activeWord)">
+        <span class="icon search-images"></span>
+      </button>
+    </div>
+    <button class="iconized" v-on:click="resetActiveWord">
+      <span class="icon reset"></span>
+    </button>
     <span class="caption all-caps">©2025 XD</span>
   <!-- </main> -->
 </template>
