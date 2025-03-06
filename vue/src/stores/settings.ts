9db95d05cfc9826cfe91ed35bd85a3695e3fb514
Cristian Díaz <xtndiaz@gmail.com>
2025-03-06 16:39:53 -0500

    Rebuild app with design-token updates and new attributes to try to disable scrolling and zooming on mobile

--- vue/src/stores/settings.ts
+++ vue/src/stores/settings.ts
@@ -5,15 +5,14 @@ export const settingsStore = defineStore('settings', {
   state: (): ISettings => {
     return {
       activeLanguage: Language.Español,
-      imageSearchUrl: 'https://duckduckgo.com/?t=ffab&q=',
-      webSearchUrl: 'https://duckduckgo.com/?t=ffab&iax=images&ia=images&q=',
+      webSearchUrl: 'https://duckduckgo.com/?t=ffab&q=',
+      imageSearchUrl: 'https://duckduckgo.com/?t=ffab&iax=images&ia=images&q=',
       languages: new Map([
         [Language.Español, {
           language: Language.Español,
           dictionaryUrl: 'https://dle.rae.es/',
         }]
       ]),
-      // mode: GameMode.Words
     }
   }
 })
