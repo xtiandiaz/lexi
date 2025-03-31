9f5b3df806ec25abdb86fa048c00a8047fca62e8
Cristian Díaz <xtndiaz@gmail.com>
2025-03-30 19:43:00 -0500

    Install project and update vite config with environment dependant base location

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: "/lexi/vue/dist",
+  base: process.env.NODE_ENV === 'production' ? '/lexi/dist/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
