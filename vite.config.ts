5e92147ec2cf9787fc709e9af42301e25e0fe5c2
Cristian Díaz <xtndiaz@gmail.com>
2025-04-05 17:57:51 -0500

    Move index file to src. Update design-tokens. Build app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  root: 'src',
   base: process.env.NODE_ENV === 'production' ? '/lexi/dist/' : '/',
   plugins: [
     vue(),
