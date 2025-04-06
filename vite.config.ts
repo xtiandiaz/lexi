c082637381c78d87edc732c5ae68a2ca39ad7b23
Cristian Díaz <xtndiaz@gmail.com>
2025-04-05 20:18:22 -0500

    Relocate index at default root folder. Build app

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  root: 'src',
+  // root: 'src',
   base: process.env.NODE_ENV === 'production' ? '/lexi/dist/' : '/',
   plugins: [
     vue(),
@@ -21,6 +21,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      // input: 'src/index.html',
       output: {
         assetFileNames: (assetInfo) => {
           if (assetInfo.names && assetInfo.names.find(name => /\.woff2$/.test(name))) {
