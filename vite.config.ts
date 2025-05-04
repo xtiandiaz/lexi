7302560319dcc4e4eb31b59f513b6111e9946036
Cristian Díaz <xtndiaz@gmail.com>
2025-05-04 17:34:35 -0500

    Slight changes to config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/lexi/dist/' : '/',
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue()
   ],
   resolve: {
     alias: {
