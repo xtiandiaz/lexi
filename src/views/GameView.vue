8eb63962aa1f8462442dc60a94cb59ef64dfed99
Cristian Díaz <xtndiaz@gmail.com>
2025-05-17 01:07:37 -0500

    Rebuild app

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -187,7 +187,7 @@ onWindowEvent('pagehide', onPageUnfocusedOrUnmounted) // for iOS
 @use '@design-tokens/palette';
 
 main {
-  background-color: greenyellow;
+  background-color: mintcream;
   
   section {
     $h-padding: 1em;
