2f3580fad9825138d3ef44b89826803345fc8ed1
Cristian Díaz <xtndiaz@gmail.com>
2025-09-02 18:13:50 +0300

    Update base config. Build app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/lexi/dist/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/lexi/' : '/',
   root: 'src',
   publicDir: '../public',
   plugins: [
